242bfeb92e26a4b1db6a2b4a332877792266d05b
Erik <erik@superannotate.com>
2021-12-06 13:03:18 +0400

    added video download script

---
+++ download_videos.py
@@ -0,0 +1,119 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+# This scripts is copied from
+# https://github.com/activitynet/ActivityNet/blob/master/Crawler/Kinetics/download.py  # noqa: E501
+# The code is licensed under the MIT licence.
+import argparse
+import os
+import ssl
+import subprocess
+
+import mmcv
+from joblib import Parallel, delayed
+
+ssl._create_default_https_context = ssl._create_unverified_context
+data_file = './sa_dataset'
+output_dir = f'{data_file}/videos'
+
+
+def download_clip(video_url,
+                  output_filename,
+                  num_attempts=5):
+    """Download a video from youtube if exists and is not blocked.
+    arguments:
+    ---------
+    video_url: str
+        Unique YouTube video identifier (11 characters)
+    output_filename: str
+        File path where the video will be stored.
+    """
+    # Defensive argument checking.
+    assert isinstance(video_url, str), 'video_identifier must be string'
+    assert isinstance(output_filename, str), 'output_filename must be string'
+
+    status = False
+
+    if not os.path.exists(output_filename):
+        command = [
+            'youtube-dl', '--quiet', '--no-warnings', '--no-check-certificate',
+            '-f', 'mp4', '-o',
+            '"%s"' % output_filename,
+            '"%s"' % (video_url)
+        ]
+        command = ' '.join(command)
+        print(command)
+        attempts = 0
+        while True:
+            try:
+                subprocess.check_output(
+                    command, shell=True, stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError:
+                attempts += 1
+                if attempts == num_attempts:
+                    return status, 'Fail'
+            else:
+                break
+    # Check if the video was successfully saved.
+    status = os.path.exists(output_filename)
+    return status, 'Downloaded'
+
+
+def download_clip_wrapper(id_url, output_dir):
+    """Wrapper for parallel processing purposes."""
+    # we do this to align with names in annotations
+    output_filename = os.path.join(output_dir, id_url[0] + '.mp4')
+    if os.path.exists(output_filename):
+        status = tuple([id_url[0], True, 'Exists'])
+        return status
+
+    downloaded, log = download_clip(id_url[1], output_filename)
+    status = tuple([id_url[0], downloaded, log])
+    return status
+
+
+def parse_activitynet_annotations(anno_file):
+    """Returns a list of YoutubeID.
+    arguments:
+    ---------
+    input_csv: str
+        Path to CSV file containing the following columns:
+          'video,numFrame,seconds,fps,rfps,subset,featureFrame'
+    returns:
+    -------
+    youtube_ids: list
+        List of all YoutubeIDs in ActivityNet.
+
+    """
+
+    data = mmcv.load(anno_file)['database']
+    idx_urls = []
+    for id, data_point in data.items():
+        idx_urls.append((id, data_point['url']))
+
+    return idx_urls
+
+
+def main(anno_file, output_dir, num_jobs=24):
+    # Reading and parsing ActivityNet.
+    youtube_ids = parse_activitynet_annotations(anno_file)
+
+    # Creates folders where videos will be saved later.
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    # Download all clips.
+    if num_jobs == 1:
+        status_list = []
+        for index in youtube_ids:
+            status_list.append(download_clip_wrapper(index, output_dir))
+    else:
+        status_list = Parallel(n_jobs=num_jobs)(
+            delayed(download_clip_wrapper)(index, output_dir)
+            for index in youtube_ids)
+
+    # Save download report.
+    mmcv.dump(status_list, 'download_report.json')
+
+
+if __name__ == '__main__':
+    video_list = f'{data_file}/sa_dataset_activitynet.json'
+    anno_file = video_list
+    main(video_list, output_dir, 24)
